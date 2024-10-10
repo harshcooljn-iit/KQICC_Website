93d293d4cf9b765b753cc10526305c3a4623fb9f
harshcooljn-iit <harshjain@kgpian.iitkgp.ac.in>
2024-10-10 14:15:47 +0530

    adding an event to schedule

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -12,6 +12,7 @@ const Schedule = () => {
                 <Schedule_card date="16/10/2024" dayTitle="Live Applications of Q Algo" venue="" time=""/>
                 <Schedule_card date="17/10/2024" dayTitle="Lecture from TCG on Applications of QI" venue="" time=""/>
                 <Schedule_card date="18/10/2024" dayTitle="Live Quantum Error correction and Machine Learning Applications" venue="" time=""/>
+                <Schedule_card date="18/10/2024" dayTitle="Live Session from Fujitsu on Challenges in Quantum Compilations + Notebook Briefings" venue="" time="7:00 pm"/>
                 <Schedule_card date="19/10/2024" dayTitle="Hackathon Notebooks"/>
             </div>
         </section>
